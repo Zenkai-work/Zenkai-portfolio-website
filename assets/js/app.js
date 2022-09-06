be1ff8c6c6c6074abd06f981a07c6fb70d497708
Dragon1207 <smileboth0805@outlook.com>
2022-09-06 10:09:58 +0900

    comb 3 pages into one page

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,8 +24,6 @@ let header = $(`
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">About Us</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="techstack.html">Skills</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
